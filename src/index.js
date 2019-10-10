555d17567ee19e43cdc1ba853e79fbb13d562ca2
Justin Perry <just.perry@gmail.com>
2019-10-10 09:41:09 +0100

    Get all use cases passing

--- src/index.js
+++ src/index.js
@@ -30,32 +30,44 @@ const createTimeInWords = (numbersAsWords: Array<string>): function => {
       past: 'past',
       to: 'to',
     };
+    const quarterMarks = [0,15,30,45];
     const durationPlural = 'minutes';
     const durationSingular = 'minute';
 
-    if (minuteNumeral === 0) {
-      output.push(numbersAsWords[hourNumeral], 'o\'clock');
-    }
+    if (quarterMarks.includes(minuteNumeral) ) {
+      if (minuteNumeral === 0) {
+        output.push(numbersAsWords[hourNumeral], 'o\'clock');
+      }
 
-    if (minuteNumeral === 15) {
-      output.push('quarter', prepositions.past, numbersAsWords[hourNumeral]);
-    }
+      if (minuteNumeral === 15) {
+        output.push('quarter', prepositions.past, numbersAsWords[hourNumeral]);
+      }
 
-    if (minuteNumeral === 30) {
-      output.push('half', prepositions.past, numbersAsWords[hourNumeral]);
-    }
+      if (minuteNumeral === 30) {
+        output.push('half', prepositions.past, numbersAsWords[hourNumeral]);
+      }
 
-    if (minuteNumeral === 45) {
-      output.push('quarter', prepositions.to, numbersAsWords[hourNumeral]);
+      if (minuteNumeral === 45) {
+        output.push('quarter', prepositions.to, numbersAsWords[hourNumeral]);
+      }
     }
-
-    if(minuteNumeral > 30 && minuteNumeral < 45 || minuteNumeral > 45 && minuteNumeral < 60) {
-      const minutesDifference = 60 - minuteNumeral;
-      const numberAsWord = minutesDifference > 20
-          ? `${numbers[20]} ${numbers[minutesDifference % 20]}`
-          : numbers[minutesDifference];
-      const duration = minutesDifference > 1? durationPlural : durationSingular;
-      output.push(numberAsWord, duration, prepositions.to, numbersAsWords[hourNumeral+1])
+    else {
+      if(minuteNumeral > 30 && minuteNumeral < 45 || minuteNumeral > 45 && minuteNumeral < 60) {
+        const minutesDifference = 60 - minuteNumeral;
+        const numberAsWord = minutesDifference > 20
+            ? `${numbers[20]} ${numbers[minutesDifference % 20]}`
+            : numbers[minutesDifference];
+        const duration = minutesDifference > 1? durationPlural : durationSingular;
+        output.push(numberAsWord, duration, prepositions.to, numbersAsWords[hourNumeral+1])
+      }
+      else if(minuteNumeral >= 1 && minuteNumeral < 45 && (minuteNumeral !== 15 || minuteNumeral !== 30)) {
+        const minutesDifference = 60 - minuteNumeral;
+        const numberAsWord = minutesDifference > 20
+            ? `${numbers[20]} ${numbers[minutesDifference % 20]}`
+            : numbers[minutesDifference];
+        const duration = minutesDifference > 1? durationPlural : durationSingular;
+        output.push(numberAsWord, duration, prepositions.past, numbersAsWords[hourNumeral])
+      }
     }
 
     return output.join(' ');
--- src/index.spec.js
+++ src/index.spec.js
@@ -26,17 +26,20 @@ const numbers = [
 const durationPlural = 'minutes';
 const durationSingular = 'minute';
 
-const hours = [...Array(12).keys()].map(index => (index += 1));
-const minutes = [...Array(60).keys()];
+const hoursInDay = [...Array(12).keys()].map(index => (index += 1));
+const minutesInHour = [...Array(60).keys()];
+
+const getNumberAsWord = number =>
+  number > 20 ? `${numbers[20]} ${numbers[number % 20]}` : numbers[number];
 
 it('should output the time in words "{hour} o\'clock" for on the hour', () => {
-  hours.forEach(hour => {
+  hoursInDay.forEach(hour => {
     return expect(timeInWords(hour, 0)).toEqual(`${numbers[hour]} o'clock`);
   });
 });
 
-it('should output the time in words "quarter past {hour}" for quarter past the hour', () => {
-  hours.forEach(hour => {
+it('should output the time in words "quarter past {hour}" for quarter to the hour', () => {
+  hoursInDay.forEach(hour => {
     return expect(timeInWords(hour, 15)).toEqual(
       `quarter past ${numbers[hour]}`
     );
@@ -44,42 +47,44 @@ it('should output the time in words "quarter past {hour}" for quarter past the h
 });
 
 it('should output the time in words "half past {hour}" for half past the hour', () => {
-  hours.forEach(hour => {
+  hoursInDay.forEach(hour => {
     return expect(timeInWords(hour, 30)).toEqual(`half past ${numbers[hour]}`);
   });
 });
 
 it('should output the time in words "quarter to {hour}" for quarter to the hour', () => {
-  hours.forEach(hour => {
+  hoursInDay.forEach(hour => {
     return expect(timeInWords(hour, 45)).toEqual(`quarter to ${numbers[hour]}`);
   });
 });
 
-it('should output the time in words "{minutes} minutes to {hour}" for the time between 31-44 and 46-59', () => {
-  const minutesRange = minutes.slice(31, 45).concat(minutes.slice(46, 60));
-  hours.forEach(hour => {
+it('should output the time in words "{minutes} minutes to {hour}" for times between 31-44 and 46-59', () => {
+  const minutesRange = minutesInHour
+    .slice(31, 45)
+    .concat(minutesInHour.slice(46, 60));
+  hoursInDay.forEach(hour => {
     minutesRange.forEach(minute => {
       const diff = 60 - minute;
-      const numberAsWord =
-        diff > 20 ? `${numbers[20]} ${numbers[diff % 20]}` : numbers[diff];
       return expect(timeInWords(hour, minute)).toEqual(
-        `${numberAsWord} ${diff > 1 ? durationPlural : durationSingular} to ${
-          numbers[hour + 1]
-        }`
+        `${getNumberAsWord(diff)} ${
+          diff > 1 ? durationPlural : durationSingular
+        } to ${numbers[hour + 1]}`
       );
     });
   });
 });
 
-it.skip('should output the time in words {minutes} past {hour} for the time between 01-29 and 31-44', () => {
-  const minutesRange = minutes.slice(1, 30).concat(minutes.slice(31, 45));
-  hours.forEach(hour => {
+it('should output the time in words "{minutes} past {hour}" for times between 01-29 and 31-44', () => {
+  const minutesRange = minutesInHour
+    .slice(1, 15)
+    .concat(minutesInHour.slice(16, 30));
+  hoursInDay.forEach(hour => {
     minutesRange.forEach(minute => {
       const diff = 60 - minute;
       return expect(timeInWords(hour, minute)).toEqual(
-        `${numbers[diff]} ${
+        `${getNumberAsWord(diff)} ${
           diff > 1 ? durationPlural : durationSingular
-        } past ${numbers[hour + 1]}`
+        } past ${numbers[hour]}`
       );
     });
   });
