11e2420248dd94e5b7d80591deca0b9256fd6886
Justin Perry <just.perry@gmail.com>
2019-10-10 11:34:00 +0100

    Handle 15, 30, 45

--- src/index.js
+++ src/index.js
@@ -24,26 +24,39 @@ const numbers = [
   'twenty'
 ];
 
+const MINUTES_LABEL_PLURAL = 'MINUTES_LABEL_PLURAL';
+const MINUTES_LABEL_SINGULAR = 'MINUTES_LABEL_SINGULAR';
+const PREPOSITION_TO = 'PREPOSITION_TO';
+const PREPOSITION_PAST = 'PREPOSITION_PAST';
+const QUARTER = 'QUARTER';
+const HALF = 'HALF';
+
 const templateStrings = {
   HOUR_MARKER: '{{hourMarker}}',
   HOURS: '{{hours}}',
   MINUTES: '{{minutes}}',
-  MINUTES_LABEL_PLURAL: '{{minutesLabelPlural}}',
-  MINUTES_LABEL_SINGULAR: '{{minutesLabelSingular}}',
-  PREPOSITION_TO: '{{prepositionTo}}',
-  PREPOSITION_PAST: '{{prepositionPast}}',
-  QUARTER: '{{quarter}}',
-  HALF: '{{half}}'
+  MINUTES_LABEL: '{{minutesLabel}}',
+  PREPOSITION: '{{preposition}}',
+  FRACTION: '{{fraction}}'
+};
+
+const templateStringsMap = {
+  MINUTES_LABEL_PLURAL: 'MINUTES_LABEL_PLURAL',
+  MINUTES_LABEL_SINGULAR: 'MINUTES_LABEL_SINGULAR',
+  PREPOSITION_TO: 'PREPOSITION_TO',
+  PREPOSITION_PAST: 'PREPOSITION_PAST',
+  QUARTER: 'QUARTER',
+  HALF: 'HALF'
 };
 
 const templateMap = {
   [templateStrings.HOUR_MARKER]: "o'clock",
-  [templateStrings.QUARTER]: 'quarter',
-  [templateStrings.HALF]: 'half',
-  [templateStrings.PREPOSITION_PAST]: 'past',
-  [templateStrings.PREPOSITION_TO]: 'to',
-  [templateStrings.MINUTES_LABEL_PLURAL]: 'minutes',
-  [templateStrings.MINUTES_LABEL_SINGULAR]: 'minute'
+  [QUARTER]: 'quarter',
+  [HALF]: 'half',
+  [PREPOSITION_PAST]: 'past',
+  [PREPOSITION_TO]: 'to',
+  [MINUTES_LABEL_PLURAL]: 'minutes',
+  [MINUTES_LABEL_SINGULAR]: 'minute'
 };
 
 const isBetween = (x: number, min: number, max: number): boolean =>
@@ -68,6 +81,24 @@ const convertNumbersToWords = (number: number): string => {
     : numbers[number];
 };
 
+const getTimeFraction = (minute: number): string => {
+  if (minute === 15 || minute === 45) {
+    return templateStringsMap.QUARTER;
+  } else if (minute === 30) {
+    return templateStringsMap.HALF;
+  }
+  return '';
+};
+
+const getPreposition = (number): string => {
+  if (isBetween(number, 31, 44) || number === 45) {
+    return templateStringsMap.PREPOSITION_TO;
+  } else if (isBetween(number, 46, 59) || number === 15 || number === 30) {
+    return templateStringsMap.PREPOSITION_PAST;
+  }
+  return '';
+};
+
 const createTimeInWords = (
   numbersAsWords: Array<string>,
   {
@@ -76,25 +107,27 @@ const createTimeInWords = (
     MINUTES,
     MINUTES_LABEL_PLURAL,
     MINUTES_LABEL_SINGULAR,
-    PREPOSITION_PAST,
-    PREPOSITION_TO,
+    PREPOSITION,
     QUARTER,
-    HALF
+    HALF,
+    FRACTION
   }: { [string]: string | number }
 ): Function => {
   return (hourNumeral: number, minuteNumeral: number): string => {
     const templateString = [];
-    const minutesAsWord = convertNumbersToWords(minuteNumeral);
-    const hoursAsWord = convertNumbersToWords(hourNumeral);
 
     if (minuteNumeral === 0) {
       templateString.push(HOURS, HOUR_MARKER);
+    } else if ([15, 30, 45].includes(minuteNumeral)) {
+      templateString.push(FRACTION, PREPOSITION, HOURS);
     }
 
     return template(templateString.join(' '), {
-      minutes: minutesAsWord,
-      hours: hoursAsWord,
-      hourMarker: templateMap[HOUR_MARKER]
+      minutes: convertNumbersToWords(minuteNumeral),
+      hours: convertNumbersToWords(hourNumeral),
+      hourMarker: templateMap[HOUR_MARKER],
+      fraction: templateMap[getTimeFraction(minuteNumeral)],
+      preposition: templateMap[getPreposition(minuteNumeral)]
     });
   };
 };
